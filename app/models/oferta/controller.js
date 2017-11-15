34d7fa37a17088380ac8da74838950d79c07d3e2
Sebastián Barrenechea <sebastian@barrenechea.cl>
2017-11-15 19:39:54 -0300

    Agregado atributos de retorno solicitados

--- app/models/oferta/controller.js
+++ app/models/oferta/controller.js
@@ -9,7 +9,7 @@ import Checkit from 'checkit'
 function GET (req, res) {
   const id = (typeof req.params.id === 'undefined' || isNaN(req.params.id) ) ? 0 : parseInt(req.params.id)
   if(id != 0) {
-    new Model({IDEN_OFERTA: id}).fetch()
+    new Model({IDEN_OFERTA: id}).fetch({withRelated: ['publicacion']})
       .then(entity => {
         if(!entity) {
           res.status(404).json({error: true, data: {message: 'Entity not found'}})
