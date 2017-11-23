50933335ab4a8f2f82b9eb05a3242a79b09e76eb
Sebastián Barrenechea <sebastian@barrenechea.cl>
2017-11-22 21:31:19 -0300

    Agregado atributo ETIQUETAS en POST y PUT de Publicacion

--- app/models/publicacion/controller.js
+++ app/models/publicacion/controller.js
@@ -1,5 +1,7 @@
 import { Model, Collection } from './model'
+import { Model as Tag } from '../etiqueta/model'
 import Checkit from 'checkit'
+import _ from 'lodash'
 
 /**
  * Obtener publicaciones.
@@ -64,6 +66,7 @@ function GET (req, res) {
  * @param {boolean} req.body.FLAG_VIGENTE - Define si la publicación está activa (opcional, por defecto false).
  * @param {boolean} req.body.FLAG_BAN - Define si la publicación está baneada (opcional, por defecto false).
  * @param {date} req.body.FECH_CREACION - Fecha de creación de la publicación (opcional, por defecto now()).
+ * @param {array} req.body.ETIQUETAS - Arreglo de strings que conformarán las etiquetas de la publicación (opcional).
  * @return {json} Publicación. En caso fallido, mensaje de error.
  */
 function POST (req, res) {
@@ -81,7 +84,21 @@ function POST (req, res) {
     FECH_CREACION:          req.body.FECH_CREACION
   }).save()
     .then(entity => {
-      res.json({error: false, data: entity.toJSON()})
+      if(req.body.ETIQUETAS && Array.isArray(req.body.ETIQUETAS)) {
+        obtainTagIDs(req.body.ETIQUETAS)
+          .then(tagIDs => {
+            entity.etiquetas().attach(tagIDs)
+              .then(() => {
+                res.json({error: false, data: entity.toJSON()})
+              })
+              .catch(err => {
+                res.status(500).json({error: true, data: {message: 'Internal error'}})
+                throw err
+              })
+          })
+      } else {
+        res.json({error: false, data: entity.toJSON()})
+      }
     }).catch(Checkit.Error, err => {
       res.status(400).json({error: true, data: err})
     }).catch(err => {
@@ -104,11 +121,12 @@ function POST (req, res) {
  * @param {boolean} req.body.FLAG_VIGENTE - Define si la publicación está activa (opcional).
  * @param {boolean} req.body.FLAG_BAN - Define si la publicación está baneada (opcional).
  * @param {date} req.body.FECH_CREACION - Fecha de creación de la publicación (opcional).
+ * @param {array} req.body.ETIQUETAS - Arreglo de strings que conformarán las etiquetas de la publicación (opcional).
  * @return {json} Mensaje de éxito o error.
  */
 function PUT (req, res) {
   new Model({IDEN_PUBLICACION: req.params.id})
-    .fetch({require: true})
+    .fetch({require: true, withRelated: ['etiquetas']})
     .then(entity => {
       entity.save({
         IDEN_EMPRENDEDOR:       (typeof req.body.IDEN_EMPRENDEDOR === 'undefined') ? entity.get('IDEN_EMPRENDEDOR') : req.body.IDEN_EMPRENDEDOR,
@@ -122,10 +140,33 @@ function PUT (req, res) {
         FLAG_VIGENTE:           (typeof req.body.FLAG_VIGENTE === 'undefined') ? entity.get('FLAG_VIGENTE') : req.body.FLAG_VIGENTE,
         FLAG_BAN:               (typeof req.body.FLAG_BAN === 'undefined') ? entity.get('FLAG_BAN') : req.body.FLAG_BAN,
         FECH_CREACION:          (typeof req.body.FECH_CREACION === 'undefined') ? entity.get('FECH_CREACION') : req.body.FECH_CREACION
-        
       })
         .then(() => {
-          res.json({error: false, data: {message: 'Entity successfully updated'}})
+          if(req.body.ETIQUETAS && Array.isArray(req.body.ETIQUETAS)) {
+            obtainTagIDs(req.body.ETIQUETAS)
+              .then(tagIDs => {
+                let attach = _.difference(tagIDs, entity.relations.etiquetas.pluck('IDEN_ETIQUETA'))
+                let detach = _.difference(entity.relations.etiquetas.pluck('IDEN_ETIQUETA'), tagIDs)
+                entity.etiquetas().attach(attach)
+                  .then(() => {
+                    entity.etiquetas().detach(detach)
+                      .then(() => {
+                        res.json({error: false, data: {message: 'Entity successfully updated'}})
+                      })
+                      .catch(err => {
+                        res.status(500).json({error: true, data: {message: 'Internal error'}})
+                        throw err
+                      })
+                  })
+                  .catch(err => {
+                    res.status(500).json({error: true, data: {message: 'Internal error'}})
+                    throw err
+                  })
+                  
+              })
+          } else {
+            res.json({error: false, data: {message: 'Entity successfully updated'}})
+          }
         })
         .catch(Checkit.Error, err => {
           res.status(400).json({error: true, data: err})
@@ -162,6 +203,40 @@ function DELETE (req, res) {
       res.status(500).json({error: true, data: {message: 'Internal error'}})
       throw err
     })
+}
+
+/**
+ * Busca ID's de Tags. En caso de no encontrar un tag, lo agregará y retornará su nuevo ID
+ * @param {*} tags Arreglo de tags a buscar
+ */
+function obtainTagIDs (tags) {
+  let promises = []
+
+  tags.forEach(tag => {
+    let promise = new Promise((resolve, reject) => {
+      return new Tag().where('NOMB_ETIQUETA', tag).fetch()
+        .then(entity => {
+          if (!entity) {
+            return new Tag({
+              NOMB_ETIQUETA: tag
+            }).save()
+              .then(data => {
+                resolve(data.attributes.IDEN_ETIQUETA)
+              })
+              .catch(err => {
+                reject(err)
+              })
+          }
+          resolve(entity.attributes.IDEN_ETIQUETA)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+    promises.push(promise)
+  })
+  
+  return Promise.all(promises)
 }
 
 /* Se exportan los métodos */
