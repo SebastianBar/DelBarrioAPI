5a3b2641bdff1b0fc72ccdab28c46ea49ad89c01
Sebastián Barrenechea <sebastian@barrenechea.cl>
2017-12-01 10:32:52 -0300

    Agregadas solicitudes del front a Emprendedor

--- app/models/emprendedor/controller.js
+++ app/models/emprendedor/controller.js
@@ -9,7 +9,7 @@ import Checkit from 'checkit'
 function GET (req, res) {
   const id = (typeof req.params.id === 'undefined' || isNaN(req.params.id) ) ? 0 : parseInt(req.params.id)
   if(id != 0) {
-    new Model({IDEN_EMPRENDEDOR: id}).fetch({withRelated: ['usuario', 'rubro', 'imagen']})
+    new Model({IDEN_EMPRENDEDOR: id}).fetch({withRelated: ['usuario', 'usuario.telefonos', 'publicaciones', 'publicaciones.imagenes', 'rubro', 'imagen']})
       .then(entity => {
         if(!entity) {
           res.status(404).json({error: true, data: {message: 'Entity not found'}})
@@ -21,7 +21,7 @@ function GET (req, res) {
         throw err
       })
   } else {
-    new Collection().fetch({withRelated: ['usuario', 'rubro', 'imagen']})
+    new Collection().fetch({withRelated: ['usuario', 'usuario.telefonos', 'publicaciones', 'publicaciones.imagenes', 'rubro', 'imagen']})
       .then(entities => {
         res.json({error: false, data: entities.toJSON()})
       }).catch(err => {
