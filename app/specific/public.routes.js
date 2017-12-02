d6f74ca2b6b659aefb66ddb8ed727577f8442797
Sebastián Barrenechea <sebastian@barrenechea.cl>
2017-12-02 11:20:49 -0300

    Nueva ruta para objetos de Index

--- app/specific/public.routes.js
+++ app/specific/public.routes.js
@@ -1,8 +1,12 @@
-import search from './search_posts'
 import express from 'express'
+import search from './search_posts'
+import index from './index'
 const app = express.Router()
 
 app.route('/search')
   .get   ((req,res) => search.GET(req, res))
+
+app.route('/index')
+  .get   ((req,res) => index.GET(req, res))
 
 export default app
